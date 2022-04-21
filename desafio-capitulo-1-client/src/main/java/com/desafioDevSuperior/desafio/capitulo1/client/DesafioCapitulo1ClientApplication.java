cb37f95b911c19d889e381f095a73f10d3ad8642
lucyfurt <lucianafurtado10@gmail.com>
2022-04-20 22:22:49 -0300

    teste2

---
+++ desafio-capitulo-1-client/src/main/java/com/desafioDevSuperior/desafio/capitulo1/client/DesafioCapitulo1ClientApplication.java
@@ -0,0 +1,13 @@
+package com.desafioDevSuperior.desafio.capitulo1.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCapitulo1ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCapitulo1ClientApplication.class, args);
+	}
+
+}
---
+++ desafio-capitulo-1-client/src/test/java/com/desafioDevSuperior/desafio/capitulo1/client/DesafioCapitulo1ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioDevSuperior.desafio.capitulo1.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCapitulo1ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
